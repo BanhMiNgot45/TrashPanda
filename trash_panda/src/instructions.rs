b90ec2c00f0619ffe57b64e2260381e6bea7128c
Steven Le <stevenhle459@gmail.com>
2021-10-02 16:11:55 -0500

    Finished main menu functionality

---
+++ trash_panda/src/instructions.rs
@@ -0,0 +1,20 @@
+use gdnative::prelude::*;
+
+#[derive(NativeClass)]
+#[inherit(Control)]
+pub struct Instructions;
+
+impl Instructions {
+    fn new(_owner: &Control) -> Self {
+        Instructions
+    }
+}
+
+#[methods]
+impl Instructions {
+    #[export]
+    fn _on_back_button_pressed(&self, owner: &Control) {
+        let tree = unsafe {Node::get_tree(owner).unwrap().assume_safe()};
+        tree.change_scene("res://Scenes/MainMenu.tscn").expect("Problem opening or instantiating Main Menu scene.");
+    }
+}
--- trash_panda/src/lib.rs
+++ trash_panda/src/lib.rs
@@ -1,10 +1,12 @@
 use gdnative::prelude::*;
 
 mod main_menu;
+mod instructions;
 
 // Function that registers all exposed classes to Godot
 fn init(handle: InitHandle) {
     handle.add_class::<main_menu::MainMenu>();
+    handle.add_class::<instructions::Instructions>();    
 }
 
 // Macro that creates the entry-points of the dynamic library.
