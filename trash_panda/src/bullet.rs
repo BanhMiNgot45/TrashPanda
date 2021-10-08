c5ed22f849843156f5e729f5c53059dc7d4f9b81
Steven Le <stevenhle459@gmail.com>
2021-10-08 16:48:49 -0500

    Beginning to refactor the Bullet and BulletSpawner code

--- trash_panda/src/bullet.rs
+++ trash_panda/src/bullet.rs
@@ -1,35 +1,12 @@
 use gdnative::api::RigidBody2D;
-use gdnative::nativescript::*;
 use gdnative::prelude::*;
-
-#[derive(ToVariant, FromVariant)]
-#[variant(enum = "i32")]
-enum Func {
-    Non,
-    Lin,
-    Quad,
-    Sin,
-    Cos,
-    Tan,
-}
-
-impl Export for Func {
-    type Hint = i32;
-    fn export_info(_hint: Option<Self::Hint>) -> ExportInfo {
-        ExportInfo::new(VariantType::I64)
-    }
-}
 
 #[derive(NativeClass)]
 #[inherit(RigidBody2D)]
 pub struct Bullet {
-    #[property]
-    speed: f32,
     direction: Vector2,
     #[property]
-    x_func: Func,
-    #[property]
-    y_func: Func,
+    speed: f32,
     #[property]
     x_coeff: f32,
     #[property]
@@ -46,10 +23,8 @@ pub struct Bullet {
 impl Bullet {
     fn new(_owner: &RigidBody2D) -> Self {
         Bullet {
-            speed: 100.0,
             direction: Vector2::new(0.0, 0.0),
-            x_func: Func::Non,
-            y_func: Func::Non,
+            speed: 100.0,
             x_coeff: 1.0,
             y_coeff: 1.0,
             theta_x_coeff: 1.0,
@@ -65,53 +40,9 @@ impl Bullet {
     #[export]
     fn _process(&mut self, owner: &RigidBody2D, delta: f32) {
         self.time += delta;
-        let inc = self.speed * delta;
-        self.direction = Vector2::new(self.para_func_x(inc), self.para_func_y(inc));
-        let position = owner.global_position() + self.direction;
-        owner.set_global_position(position);
     }
 
-    fn para_func_x(&self, inc: f32) -> f32 {
-        match self.x_func {
-            Func::Non => self.x_coeff + inc,
-            Func::Lin => (self.x_coeff + inc) * self.theta_x_coeff * self.time,
-            Func::Quad => (self.x_coeff + inc) * self.theta_x_coeff * self.time.powi(2),
-            Func::Sin => {
-                let theta = self.start_theta + self.theta_x_coeff * self.time;
-                (self.x_coeff + inc) * theta.sin()
-            }
-            Func::Cos => {
-                let theta = self.start_theta + self.theta_x_coeff * self.time;
-                (self.x_coeff + inc) * theta.cos()
-            }
-            Func::Tan => {
-                let theta = self.start_theta + self.theta_x_coeff * self.time;
-                (self.x_coeff + inc) * theta.tan()
-            }
-        }
-    }
-
-    fn para_func_y(&self, inc: f32) -> f32 {
-        match self.y_func {
-            Func::Non => self.y_coeff,
-            Func::Lin => self.y_coeff * self.theta_y_coeff * self.time,
-            Func::Quad => self.y_coeff * self.theta_y_coeff * self.time.powi(2),
-            Func::Sin => {
-                let theta = self.start_theta + self.theta_y_coeff * self.time;
-                (self.y_coeff + inc) * theta.sin()
-            }
-            Func::Cos => {
-                let theta = self.start_theta + self.theta_y_coeff * self.time;
-                (self.y_coeff + inc) * theta.cos()
-            }
-            Func::Tan => {
-                let theta = self.start_theta + self.theta_y_coeff * self.time;
-                (self.y_coeff + inc) * theta.tan()
-            }
-        }
-    }
-
-    fn _on_bullet_body_entered(&self, _owner: &RigidBody2D, body: Node) {
-        body.queue_free();
+    fn _on_bullet_body_entered(&self, owner: &RigidBody2D, _body: Node) {
+        owner.queue_free();
     }
 }
--- trash_panda/src/lib.rs
+++ trash_panda/src/lib.rs
@@ -1,7 +1,6 @@
 use gdnative::prelude::*;
 
 mod bullet;
-mod bullet_manager;
 mod instructions;
 mod main_menu;
 mod racoon;
@@ -12,7 +11,6 @@ fn init(handle: InitHandle) {
     handle.add_class::<instructions::Instructions>();
     handle.add_class::<racoon::Racoon>();
     handle.add_class::<bullet::Bullet>();
-    handle.add_class::<bullet_manager::BulletManager>();
 }
 
 // Macro that creates the entry-points of the dynamic library.
